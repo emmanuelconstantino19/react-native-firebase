ae77cc0b781cab7aec95ee0152b8612f57c77c2f
emmanuelconstantino19 <emmanuelconstantino19@gmail.com>
2021-09-03 14:28:42 +0800

    Implemented Academic Records Page

--- App.js
+++ App.js
@@ -1,41 +1,44 @@
-import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react'
-import { firebase } from './src/firebase/config'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
-import {decode, encode} from 'base-64'
-import { Text, TouchableOpacity } from 'react-native'
-import { MaterialIcons } from '@expo/vector-icons';
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode }
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { firebase } from "./src/firebase/config";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
+import { decode, encode } from "base-64";
+import { Text, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const usersRef = firebase.firestore().collection('users');
-    const authListener = firebase.auth().onAuthStateChanged(user => {
+    const usersRef = firebase.firestore().collection("users");
+    const authListener = firebase.auth().onAuthStateChanged((user) => {
       // user logged in
       if (user) {
         usersRef
           .doc(user.uid)
           .get()
           .then((document) => {
-            const userData = document.data()
-            setLoading(false)
-            setUser(userData)
+            const userData = document.data();
+            setLoading(false);
+            setUser({ uid: user.uid, ...userData });
           })
           .catch((error) => {
-            setLoading(false)
+            setLoading(false);
           });
       } else {
-        setUser(null)
-        setLoading(false)
+        setUser(null);
+        setLoading(false);
       }
     });
 
@@ -43,32 +46,30 @@ export default function App() {
   }, []);
 
   if (loading) {
-    return (
-      <></>
-    )
+    return <></>;
   }
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        { user ? (
+        {user ? (
           <>
-            <Stack.Screen 
-              name="Home" 
+            <Stack.Screen
+              name="Academic Records"
               options={{
                 headerRight: () => (
                   <TouchableOpacity
                     onPress={() => firebase.auth().signOut()}
                     title="Info"
                     color="#fff"
-                    style={{marginRight: 20}}
+                    style={{ marginRight: 20 }}
                   >
                     <MaterialIcons name="logout" size={24} color="black" />
                   </TouchableOpacity>
                 ),
               }}
             >
-              {props => <HomeScreen {...props} extraData={user} />}
+              {(props) => <HomeScreen {...props} extraData={user} />}
             </Stack.Screen>
             {/* <Stack.Screen name="Cash In" component={CashInScreen} />
             <Stack.Screen name="Pay Bills" component={PayBillsScreen} />
@@ -84,8 +85,16 @@ export default function App() {
           </>
         ) : (
           <>
-            <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="Registration" component={RegistrationScreen} options={{headerShown: false}}/>
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Registration"
+              component={RegistrationScreen}
+              options={{ headerShown: false }}
+            />
           </>
         )}
       </Stack.Navigator>
--- src/screens/RegistrationScreen/styles.js
+++ src/screens/RegistrationScreen/styles.js
@@ -1,70 +1,68 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    title: {
-
-    },
-    logo: {
-        flex: 1,
-        height: 120,
-        width: 130,
-        alignSelf: "center",
-        margin: 30
-    },
-    input: {
-        height: 48,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        paddingLeft: 16
-    },
-    button: {
-        backgroundColor: '#006400',
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-        height: 48,
-        borderRadius: 5,
-        alignItems: "center",
-        justifyContent: 'center'
-    },
-    buttonTitle: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: "bold"
-    },
-    footerView: {
-        flex: 1,
-        alignItems: "center",
-        marginTop: 20
-    },
-    footerText: {
-        fontSize: 16,
-        color: '#2e2e2d'
-    },
-    footerLink: {
-        color: "#006400",
-        fontWeight: "bold",
-        fontSize: 16
-    },
-    text: {
-        textAlign:"center",
-    },
-    mainTitle: {
-        fontSize: 25,
-        marginBottom: 10,
-        fontWeight: "bold"
-    },
-    subTitle: {
-        fontSize: 16,
-        marginBottom: 10,
-    }
-})
+  container: {
+    flex: 1,
+    // alignItems: 'center'
+  },
+  title: {},
+  logo: {
+    flex: 1,
+    height: 120,
+    width: 130,
+    alignSelf: "center",
+    margin: 30,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  button: {
+    backgroundColor: "#006400",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+  },
+  footerLink: {
+    color: "#006400",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  text: {
+    textAlign: "center",
+  },
+  mainTitle: {
+    fontSize: 25,
+    marginBottom: 10,
+    fontWeight: "bold",
+  },
+  subTitle: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+});
